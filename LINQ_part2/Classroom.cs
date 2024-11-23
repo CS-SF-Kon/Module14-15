5334695dd0ab9a9222273cfeaf5da5f5bb9db505
CS-SF-Kon <vladislav.konovalov@evraz.com>
2024-11-23 16:18:42 +0700

    LINQ_part2: final task

---
+++ LINQ_part2/Classroom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_part2;
+
+/// <summary>
+/// Класс "класс", содержащий перечень студентов в виде строкового списка
+/// </summary>
+internal class Classroom
+{
+    public List<string> Students = new List<string>();
+}
---
+++ LINQ_part2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace LINQ_part2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var classes = new[]
+        {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+        var allStudents = GetAllStudents(classes);
+
+        Console.WriteLine(string.Join(" ", allStudents));
+    }
+
+    /// <summary>
+    /// Метод, возвращающий массив имён студентов из всех экземпляров, содержащихся в оригинальном массиве
+    /// </summary>
+    /// <param name="classes"></param>
+    /// <returns></returns>
+    static string[] GetAllStudents(Classroom[] classes)
+    {
+        string[] names = classes.SelectMany(x => x.Students).ToArray();
+
+        // ВАУ, получается, действительно почти любую операцию можно реализовать и через методы расширения, и через операторы запросов. Осталось только выучить...
+
+        //string[] names = (from c in classes
+        //                  from v in c.Students
+        //                  select v).ToArray
+
+        return names;
+    }
+}
